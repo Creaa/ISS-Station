248c719fc7becc905315565eb971d3eefcba42c2
Sebastian Kondratowicz <feeverpl@gmail.com>
2019-03-18 23:52:22 +0100

    build version added

---
+++ build/precache-manifest.7906463e0c7de9e1f60bff0681af8433.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "520a24fca41a215d6b0620f470ce8488",
+    "url": "./static/media/station.520a24fc.png"
+  },
+  {
+    "revision": "6dd445da3aaca96678ee23de1a6453f5",
+    "url": "./static/media/earth-map.6dd445da.svg"
+  },
+  {
+    "revision": "ef2c8acb44041d721dd45263e2540adc",
+    "url": "./static/media/digitaldream-webfont.ef2c8acb.woff2"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "046017bcc8c0cc2e6160",
+    "url": "./static/js/main.815cf06b.chunk.js"
+  },
+  {
+    "revision": "33ee1243e25fdf0903db",
+    "url": "./static/js/2.c0793905.chunk.js"
+  },
+  {
+    "revision": "046017bcc8c0cc2e6160",
+    "url": "./static/css/main.94a603ee.chunk.css"
+  },
+  {
+    "revision": "857444ad94afd37cc660486a22f23854",
+    "url": "./index.html"
+  }
+];
